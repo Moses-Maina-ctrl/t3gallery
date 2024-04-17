f8e0da9d4dcd3d76f0fb2b9101df93e7ed03d6ff
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-04-17 21:39:02 +0300

    Fix Eslint error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <h1>Welcome to Nene's Gallery !!!</h1>
+        Welcome to Nene's Gallery !!!
       </main>
     </>
   );
